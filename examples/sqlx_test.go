38944708fadd773f9124710f3d01331027b12710
Fergus Strange <fergus.strange@infinityworks.com>
2019-11-22 14:05:22 +1100

    Add an extra test to check if it's working well in actions.

--- examples/sqlx_test.go
+++ examples/sqlx_test.go
@@ -2,11 +2,11 @@ package examples
 
 import (
 	"github.com/fergusstrange/embedded-postgres"
+	"github.com/pressly/goose"
 	"os"
 
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
-	"github.com/pressly/goose"
 	"testing"
 )
 
@@ -15,27 +15,20 @@ func TestMain(m *testing.M) {
 	if err := database.Start(); err != nil {
 		panic(err)
 	}
-
+	db, err := connect()
+	if err != nil {
+		panic(err)
+	}
+	if errMigration := goose.Up(db.DB, "./migrations"); errMigration != nil {
+		panic(errMigration)
+	}
 	exitCode := m.Run()
-
 	database.Stop()
-
 	os.Exit(exitCode)
 }
 
-func Test_Migration(t *testing.T) {
-	db, err := sqlx.Connect("postgres", "host=localhost port=5432 user=postgres password=postgres dbname=postgres sslmode=disable")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if errMigration := goose.Up(db.DB, "./migrations"); errMigration != nil {
-		t.Fatal(err)
-	}
-}
-
 func Test_SelectOne(t *testing.T) {
-	db, err := sqlx.Connect("postgres", "host=localhost port=5432 user=postgres password=postgres dbname=postgres sslmode=disable")
+	db, err := connect()
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -49,4 +42,20 @@ func Test_SelectOne(t *testing.T) {
 	if len(rows) != 1 {
 		t.Fatal("Expected one row returned")
 	}
+}
+
+func Test_Insert(t *testing.T) {
+	db, err := connect()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if _, err := db.Exec(`INSERT INTO tom_beresford_beer_catalogue (name, consumed, rating) VALUES ('kernal', true, 99.99)`); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func connect() (*sqlx.DB, error) {
+	db, err := sqlx.Connect("postgres", "host=localhost port=5432 user=postgres password=postgres dbname=postgres sslmode=disable")
+	return db, err
 }
