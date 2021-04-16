a50b55071c9751e6a9253764a270d72533e59010
Christoph Hartmann <chris@lollyrock.com>
2021-04-17 00:48:36 +0200

    ⭐️ alpine binary support (#23)
    * Add alpine linux coverage
    * ⭐️ alpine binary support
    Co-authored-by: Fergus Strange <fergus@sitehive.co>

--- version_strategy.go
+++ version_strategy.go
@@ -1,6 +1,9 @@
 package embeddedpostgres
 
-import "runtime"
+import (
+	"os"
+	"runtime"
+)
 
 // VersionStrategy provides a strategy that can be used to determine which version of Postgres should be used based on
 // the operating system, architecture and desired Postgres version.
@@ -8,6 +11,16 @@ type VersionStrategy func() (operatingSystem string, architecture string, postgr
 
 func defaultVersionStrategy(config Config) VersionStrategy {
 	return func() (operatingSystem, architecture string, version PostgresVersion) {
-		return runtime.GOOS, runtime.GOARCH, config.version
+		goos := runtime.GOOS
+		arch := runtime.GOARCH
+
+		// use alpine specific build
+		if goos == "linux" {
+			if _, err := os.Stat("/etc/alpine-release"); err == nil {
+				arch += "-alpine"
+			}
+		}
+
+		return goos, arch, config.version
 	}
 }
