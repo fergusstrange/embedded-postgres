bba5d0ea4081a5093daf9faed4ee10f35bc1207e
Fergus Strange <fergus@sitehive.co>
2021-09-15 17:11:10 +1000

    Try ditching manual clean up

--- platform-test/platform_test.go
+++ platform-test/platform_test.go
@@ -4,91 +4,66 @@ import (
 	"database/sql"
 	"fmt"
 	"io/ioutil"
-	"log"
-	"os"
 	"path/filepath"
 	"strconv"
 	"strings"
 	"testing"
-	"time"
 
 	embeddedpostgres "github.com/fergusstrange/embedded-postgres"
 )
 
 func Test_AllMajorVersions(t *testing.T) {
 	allVersions := []embeddedpostgres.PostgresVersion{
-		embeddedpostgres.V9,
-		embeddedpostgres.V10,
-		embeddedpostgres.V11,
-		embeddedpostgres.V12,
 		embeddedpostgres.V13,
+		embeddedpostgres.V12,
+		embeddedpostgres.V11,
+		embeddedpostgres.V10,
+		embeddedpostgres.V9,
 	}
 
-	tempExtractLocation, err := ioutil.TempDir("", "embedded_postgres_go_tests")
-	if err != nil {
-		t.Fatal(err)
-	}
+	tempExtractLocation := t.TempDir()
 
 	for i, v := range allVersions {
 		testNumber := i
 		version := v
+		t.Run(fmt.Sprintf("MajorVersion_%s", version), func(t *testing.T) {
+			port := uint32(5555 + testNumber)
+			runtimePath := filepath.Join(tempExtractLocation, strconv.Itoa(testNumber))
+			database := embeddedpostgres.NewDatabase(embeddedpostgres.DefaultConfig().
+				Version(version).
+				Port(port).
+				RuntimePath(runtimePath))
 
-		port := uint32(5555 + testNumber)
-		runtimePath := filepath.Join(tempExtractLocation, strconv.Itoa(testNumber))
-		database := embeddedpostgres.NewDatabase(embeddedpostgres.DefaultConfig().
-			Version(version).
-			Port(port).
-			RuntimePath(runtimePath))
+			if err := database.Start(); err != nil {
+				shutdownDBAndFail(t, err, database, version)
+			}
 
-		if err := database.Start(); err != nil {
-			shutdownDBAndFail(t, err, database, version)
-		}
+			db, err := connect(port)
+			if err != nil {
+				shutdownDBAndFail(t, err, database, version)
+			}
 
-		db, err := connect(port)
-		if err != nil {
-			shutdownDBAndFail(t, err, database, version)
-		}
+			rows, err := db.Query("SELECT 1")
+			if err != nil {
+				shutdownDBAndFail(t, err, database, version)
+			}
+			if err := rows.Close(); err != nil {
+				shutdownDBAndFail(t, err, database, version)
+			}
 
-		rows, err := db.Query("SELECT 1")
-		if err != nil {
-			shutdownDBAndFail(t, err, database, version)
-		}
+			if err := db.Close(); err != nil {
+				shutdownDBAndFail(t, err, database, version)
+			}
 
-		if err := rows.Close(); err != nil {
-			shutdownDBAndFail(t, err, database, version)
-		}
+			if err := database.Stop(); err != nil {
+				t.Fatal(err)
+			}
 
-		if err := db.Close(); err != nil {
-			shutdownDBAndFail(t, err, database, version)
-		}
-
-		if err := database.Stop(); err != nil {
-			t.Fatal(err)
-		}
-
-		if err := checkPgVersionFile(filepath.Join(runtimePath, "data"), version); err != nil {
-			t.Fatal(err)
-		}
+			if err := checkPgVersionFile(filepath.Join(runtimePath, "data"), version); err != nil {
+				t.Fatal(err)
+			}
+		})
 	}
-
-	if err := purgeTestDataOrWait(tempExtractLocation, 0); err != nil {
-		t.Fatal(err)
-	}
-}
-
-func purgeTestDataOrWait(tempExtractLocation string, attempts int) error {
-	if err := os.RemoveAll(tempExtractLocation); err != nil {
-		if attempts < 30 {
-			log.Println("waiting for tests to clean up...")
-			time.Sleep(1 * time.Second)
-
-			return purgeTestDataOrWait(tempExtractLocation, attempts+1)
-		}
-
-		return err
-	}
-
-	return nil
 }
 
 func shutdownDBAndFail(t *testing.T, err error, db *embeddedpostgres.EmbeddedPostgres, version embeddedpostgres.PostgresVersion) {
