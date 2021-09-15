e3576f87d3b2e7d8dc7a4c4010fe50f5e1675bba
Fergus Strange <fergus@sitehive.co>
2021-09-15 15:22:01 +1000

    Reorder tests

--- platform-test/platform_test.go
+++ platform-test/platform_test.go
@@ -17,11 +17,11 @@ import (
 
 func Test_AllMajorVersions(t *testing.T) {
 	allVersions := []embeddedpostgres.PostgresVersion{
-		embeddedpostgres.V13,
-		embeddedpostgres.V12,
-		embeddedpostgres.V11,
-		embeddedpostgres.V10,
 		embeddedpostgres.V9,
+		embeddedpostgres.V10,
+		embeddedpostgres.V11,
+		embeddedpostgres.V12,
+		embeddedpostgres.V13,
 	}
 
 	tempExtractLocation, err := ioutil.TempDir("", "embedded_postgres_go_tests")
