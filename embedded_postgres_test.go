61524db132a85134717fe12dcc75ffb1756b1af0
Josh Giles <jgiles@paxos.com>
2023-02-21 22:45:34 -0500

    Fix error quoting expectations.

--- embedded_postgres_test.go
+++ embedded_postgres_test.go
@@ -231,7 +231,7 @@ func Test_ErrorWhenCannotStartPostgresProcess(t *testing.T) {
 
 	err = database.Start()
 
-	assert.EqualError(t, err, fmt.Sprintf(`could not start postgres using %s/bin/pg_ctl start -w -D %s/data -o "-p 5432"`, extractPath, extractPath))
+	assert.EqualError(t, err, fmt.Sprintf(`could not start postgres using %s/bin/pg_ctl start -w -D %s/data -o -p 5432`, extractPath, extractPath))
 }
 
 func Test_CustomConfig(t *testing.T) {
