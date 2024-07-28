3b10d84862053f3e04d20f3f5ad098e28dd8fc68
ferguss <fergus@sitehvie.co>
2024-07-28 11:06:28 +1000

    Upgrade versions

--- config.go
+++ config.go
@@ -153,11 +153,11 @@ type PostgresVersion string
 
 // Predefined supported Postgres versions.
 const (
-	V16 = PostgresVersion("16.2.0")
-	V15 = PostgresVersion("15.6.0")
-	V14 = PostgresVersion("14.11.0")
-	V13 = PostgresVersion("13.14.0")
-	V12 = PostgresVersion("12.17.0")
+	V16 = PostgresVersion("16.3.0")
+	V15 = PostgresVersion("15.7.0")
+	V14 = PostgresVersion("14.12.0")
+	V13 = PostgresVersion("13.15.0")
+	V12 = PostgresVersion("12.19.0")
 	V11 = PostgresVersion("11.22.0")
 	V10 = PostgresVersion("10.23.0")
 	V9  = PostgresVersion("9.6.24")
